9b5ef4704cb1aa4f61424a9f53646823b32f1d23
ramankooner <ramankooner9@gmail.com>
2019-01-31 17:42:38 -0800

    Changed authenticate:

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,15 +3,12 @@ const keys = require('./../config/config');
 var admin = (req, res, next) => {
 
   // Check for access code. Set key to the access code label
-
   var key = req.body.accessCode;
 
   if(key === process.env.ACCESS_CODE) {
     next();
   } else {
-    return res.status(401).send({
-      console.log('Error. Not authorized.');
-      )
-    });
+    res.redirect('/');
+    console.log('Error. Not authorized.');
   }
 }
